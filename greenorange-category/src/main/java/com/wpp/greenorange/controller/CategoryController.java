27020d57e8cbcea36a59feeac2bdfcce86352238
dxtgit <2454167762@qq.com>
2020-09-07 12:21:26 +0800

    邓晓涛：首页商品分类渲染完成

--- greenorange-category/src/main/java/com/wpp/greenorange/controller/CategoryController.java
+++ greenorange-category/src/main/java/com/wpp/greenorange/controller/CategoryController.java
@@ -5,12 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.pagehelper.PageInfo;
 import com.wpp.greenorange.domain.Category;
 import com.wpp.greenorange.service.CategoryService;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 商品类目(Category)表控制层
@@ -20,7 +23,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/category")
-public class CategoryController {
+public class CategoryController implements ApplicationRunner {
     /**
      * 服务对象
      */
@@ -28,28 +31,17 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @Resource
-    private RedisTemplate<String,String> redisTemplate;
-    @RequestMapping("/test")
-    public List test() throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-//        List<Category> categorys = categoryService.findCategorys(0);
-//        String categorysStr = mapper.writeValueAsString(categorys);
-//        redisTemplate.opsForValue().set("categorys",categorysStr);
-        String categorys = redisTemplate.opsForValue().get("categorys");
-        return mapper.readValue(categorys, List.class);
-    }
+    private RedisTemplate<String, String> redisTemplate;
 
     /**
-     *
      * @param pageNum
      * @param pageSize
      * @return
      */
     @RequestMapping("/findCategoryLimit")
-    public PageInfo<Category> findCategoryLimit(Integer pageNum, Integer pageSize){
+    public PageInfo<Category> findCategoryLimit(Integer pageNum, Integer pageSize) {
         return categoryService.findCategoryLimit(pageNum, pageSize);
     }
-
 
 
     /**
@@ -61,6 +53,21 @@ public class CategoryController {
     @RequestMapping("/getOne")
     public Category getOne(Integer id) {
         return this.categoryService.findById(id);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Map> categorys = categoryService.findCategorys();
+        String categorysStr = mapper.writeValueAsString(categorys);
+        redisTemplate.opsForValue().set("categorys",categorysStr);
+    }
+
+    @RequestMapping("/findCategorys")
+    public List<Map> findCategorys() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String categorys = redisTemplate.opsForValue().get("categorys");
+        return mapper.readValue(categorys, List.class);
     }
 
 }
